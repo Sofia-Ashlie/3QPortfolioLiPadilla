e914fcdfeb70a9db670c9a8144d77ec5da86d030
Sofia-Ashlie <b2029saqpadilla@pshs.edu.ph>
2026-03-09 08:50:42 +0800

    rr6r6rr7rr7rr7r7r7

--- public/movie.js
+++ public/movie.js
@@ -1,3 +1,43 @@
+const loginForm = document.getElementById("Add");
+const inputTitle = document.getElementById("Title");
+const inputYear = document.getElementById("Year");
+const inputGenre = document.getElementById("Genre");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 0: {title: "How to Train Your Dragon", year: "2025", genre: "Comedy", rating: "5"}
     genre: "Comedy"
     rating: "5"
